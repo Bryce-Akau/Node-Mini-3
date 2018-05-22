68ce6cfb9f0f992b2b000fa1aa81cd07c9633144
Bryson <bryce.akau08@gmail.com>
2018-05-22 12:58:57 -0700

    finished

--- server/middlewares/filter.js
+++ server/middlewares/filter.js
@@ -3,7 +3,7 @@ const notAllowed = ['dookie', 'punani', 'snake']
 module.exports = function( req, res, next){
     while ( notAllowed.find( word => req.body.text.includes(word) ) ) {
         const badWord = notAllowed.find( word => req.body.text.includes(word) );
-        req.body.text = req.body.text.replace( badWord, '@$$'.repeat( badWord.length ) );
+        req.body.text = req.body.text.replace( badWord, 'thats a bad word. imma tell yo mama '.repeat( badWord.length ) );
       }
     next()
 }
